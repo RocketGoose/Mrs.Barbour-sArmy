a10f4c3ea85170ac17dfb4d4fef09e6bbf3f26fb
sas365 <94610862+sas365@users.noreply.github.com>
2022-04-23 15:43:18 +0100

    Audio Subtitle Update
    Audio Subtitle Update 01

---
+++ Mrs.BarboursArmy/Assets/Scripts/AudioEffect.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEffect : MonoBehaviour
+{   
+
+    AudioSource myAudioSource;
+
+    bool isPlaying;
+
+
+     void OnEnable()
+    {
+        ActivateTrigger.OnTrigger += PlayAudio;
+
+    }
+
+    void OnDisable()
+    {
+        ActivateTrigger.OnTrigger -= PlayAudio;
+    }
+    
+     void PlayAudio(Vector3 triggerPoint, string audioObject )
+    {
+        string cube = gameObject.name;
+
+        
+        if (audioObject.Contains(cube) == true && isPlaying == false)
+        {
+
+            {
+                    isPlaying = true;
+                    myAudioSource.Play();
+                    Debug.Log("Audio is playing");
+            }
+                        
+
+        }
+       /* else if  (audioObject.Contains(cube) == true && isPlaying == true)
+        {
+
+            {
+                isPlaying = false;
+                myAudioSource.Stop();
+                Debug.Log("Audio stopped");
+                        
+            }
+
+        }*/
+      
+    }
+    void Start()
+    {
+      
+        myAudioSource = GetComponent<AudioSource>();
+        isPlaying = false;
+       
+    }
+
+
+}
